d3edeaea04b8c9ebe33e30380742211e0e478b06
hauts5124 <hauts5124@gmail.com>
2025-08-11 17:06:51 +0900

    update

--- app/chapters/[id]/page.tsx
+++ app/chapters/[id]/page.tsx
@@ -19,8 +19,8 @@ export async function generateStaticParams() {
   return chapters.map((ch: Chapter) => ({ id: ch.id.toString() }));
 }
 
-export default async function ChapterDetail({ params }: { params: Params }) {
-  const { id } = params;
+export default async function ChapterDetail({ params }: { params: Promise<Params> }) {
+  const { id } = await params;
   const chapterId = parseInt(id, 10);
   const chapter = chapters.find(ch => ch.id === chapterId);
   if (!chapter) {
